203025d1260663a880df6326e238283141b022de
qichi91 <potatoqp@gmail.com>
2024-08-29 20:57:10 +0900

    first commit

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi_pagination import Page, add_pagination, paginate, Params
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+
+from app.routes import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+# CORS設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
